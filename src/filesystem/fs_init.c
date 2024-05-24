bab14fa7556d023520cb2f21f7b1aeb3afb85b2c
Hiroyuki OYAMA <oyama@module.jp>
2024-05-25 01:33:53 +0900

    fixed typo

--- src/filesystem/fs_init.c
+++ src/filesystem/fs_init.c
@@ -8,7 +8,7 @@ bool __attribute__((weak)) fs_init(void) {
     filesystem_t *fs = filesystem_littlefs_create(500, 16);
     blockdevice_t *device = blockdevice_flash_create(PICO_FLASH_SIZE_BYTES - DEFAULT_FS_SIZE, 0);
 
-    int err = fs_mount("/", fs, device) == 0;
+    int err = fs_mount("/", fs, device);
     if (err != 0) {
         err = fs->format(fs, device);
         if (err != 0) {
