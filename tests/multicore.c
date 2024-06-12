a3ad5213122439d827222a44711a0c322d5330c6
Hiroyuki OYAMA <oyama@module.jp>
2024-06-12 14:21:01 +0900

    Fix #32 SD card CRC checks

--- tests/multicore.c
+++ tests/multicore.c
@@ -1,3 +1,6 @@
+/*
+ * NOTE: When operating Flash memory from core1, it must be run from RAM.
+ */
 #include <assert.h>
 #include <hardware/clocks.h>
 #include <hardware/flash.h>
@@ -16,7 +19,7 @@
 
 #define COLOR_GREEN(format)  ("\e[32m" format "\e[0m")
 
-#define TEST_FILE_SIZE      32 * 1024;
+#define TEST_FILE_SIZE      (320 * 1024)
 #define FLASH_START_AT      (0.5 * 1024 * 1024)
 #define FLASH_LENGTH_ALL    0
 
@@ -34,7 +37,7 @@ struct combination_map {
 #endif
 
 
-static struct combination_map combination[NUM_COMBINATION];
+static struct combination_map combination[NUM_COMBINATION] = {0};
 
 static void init_filesystem_combination(void) {
     blockdevice_t *flash = blockdevice_flash_create(PICO_FLASH_SIZE_BYTES - PICO_FS_DEFAULT_SIZE,
@@ -45,7 +48,7 @@ static void init_filesystem_combination(void) {
                                               PICO_DEFAULT_SPI_RX_PIN,
                                               PICO_DEFAULT_SPI_SCK_PIN,
                                               PICO_DEFAULT_SPI_CSN_PIN,
-                                              10 * MHZ,
+                                              24 * MHZ,
                                               true);
 #endif
     filesystem_t *fat = filesystem_fat_create();
@@ -76,7 +79,7 @@ static void cleanup_combination(void) {
 #endif
 }
 
-static void test_printf(const char *format, ...) {
+static size_t test_printf(const char *format, ...) {
     va_list args;
     va_start(args, format);
     int n = vprintf(format, args);
@@ -85,6 +88,22 @@ static void test_printf(const char *format, ...) {
     printf(" ");
     for (size_t i = 0; i < 50 - (size_t)n; i++)
         printf(".");
+    return (size_t)n;
+}
+
+static void print_progress(const char *label, size_t current, size_t total) {
+    int num_dots = (int)((double)current / total * (50 - strlen(label)));
+    int num_spaces = (50 - strlen(label)) - num_dots;
+
+    printf("\r%s ", label);
+    for (int i = 0; i < num_dots; i++) {
+        printf(".");
+    }
+    for (int i = 0; i < num_spaces; i++) {
+        printf(" ");
+    }
+    printf(" %zu/%zu bytes", current, total);
+    fflush(stdout);
 }
 
 static void __not_in_flash_func(test_write_read_two_files_core1)(void) {
@@ -131,9 +150,13 @@ static void __not_in_flash_func(test_write_read_two_files_core1)(void) {
 }
 
 static void test_write_read_two_files(void) {
-    test_printf("Write then read");
+    const char *label = "Write then read";
 
+    /* NOTE: When running with pico-sdk 1.5.1 and openocd, core1 needs to reset and sleep.
+     *       https://forums.raspberrypi.com/viewtopic.php?t=349795
+     */
     multicore_reset_core1();
+    sleep_ms(100);
     multicore_launch_core1(test_write_read_two_files_core1);
 
     int fd = open("/core0", O_WRONLY|O_CREAT);
@@ -148,6 +171,8 @@ static void test_write_read_two_files(void) {
         ssize_t write_size = write(fd, buffer, chunk);
         assert(write_size != -1);
         remind = remind - write_size;
+
+        print_progress(label, TEST_FILE_SIZE - remind, TEST_FILE_SIZE * 2);
     }
 
     int err = close(fd);
@@ -168,6 +193,7 @@ static void test_write_read_two_files(void) {
             assert(buffer[i] == r);
         }
         remind = remind - read_size;
+        print_progress(label, TEST_FILE_SIZE * 2 - remind, TEST_FILE_SIZE * 2);
     }
     err = close(fd);
     assert(err == 0);
@@ -175,7 +201,7 @@ static void test_write_read_two_files(void) {
     uint32_t result = multicore_fifo_pop_blocking();
     assert(result == 1);
 
-    printf(COLOR_GREEN("ok\n"));
+    printf(COLOR_GREEN(" ok\n"));
 }
 
 
@@ -218,14 +244,17 @@ static void __not_in_flash_func(test_write_while_read_two_files_core1)(void) {
     multicore_fifo_push_blocking(1); // read finish
     while (1)
         tight_loop_contents();
-
 }
 
 
 static void test_write_while_read_two_files(void) {
-    test_printf("Write while read");
+    const char *label = "Write while read";
 
+    /* NOTE: When running with pico-sdk 1.5.1 and openocd, core1 needs to reset and sleep.
+     *       https://forums.raspberrypi.com/viewtopic.php?t=349795
+     */
     multicore_reset_core1();
+    sleep_ms(100);
     multicore_launch_core1(test_write_while_read_two_files_core1);
 
     int fd = open("/core0", O_RDONLY);
@@ -244,6 +273,7 @@ static void test_write_while_read_two_files(void) {
             assert(buffer[i] == r);
         }
         remind = remind - read_size;
+        print_progress(label, TEST_FILE_SIZE - remind, TEST_FILE_SIZE * 2);
     }
     int err = close(fd);
     assert(err == 0);
@@ -263,6 +293,7 @@ static void test_write_while_read_two_files(void) {
         ssize_t write_size = write(fd, buffer, chunk);
         assert(write_size != -1);
         remind = remind - write_size;
+        print_progress(label, TEST_FILE_SIZE * 2 - remind, TEST_FILE_SIZE * 2);
     }
     err = close(fd);
     assert(err == 0);
@@ -271,7 +302,7 @@ static void test_write_while_read_two_files(void) {
     result = multicore_fifo_pop_blocking();
     assert(result == 1);
 
-    printf(COLOR_GREEN("ok\n"));
+    printf(COLOR_GREEN(" ok\n"));
 }
 
 
@@ -283,9 +314,7 @@ static void setup(filesystem_t *fs, blockdevice_t *device) {
 }
 
 static void cleanup() {
-    int err = fs_reformat("/");
-    assert(err == 0);
-    err = fs_unmount("/");
+    int err = fs_unmount("/");
     assert(err == 0);
 }
 
@@ -295,10 +324,10 @@ int main(void) {
 
     init_filesystem_combination();
     for (size_t i = 0; i < NUM_COMBINATION; i++) {
-        struct combination_map setting = combination[i];
-        printf("%s:\n", setting.label);
+        struct combination_map *setting = &combination[i];
+        printf("%s:\n", setting->label);
 
-        setup(setting.filesystem, setting.device);
+        setup(setting->filesystem, setting->device);
 
         test_write_read_two_files();
         test_write_while_read_two_files();
